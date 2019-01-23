50522431317f07f6de36d321d4c5e5ff706462dc
Alexandre Anastassov <a.anastassov@iaea.org>
2019-01-23 15:21:05 +0100

    final version - typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/helloworld';
 
 class App extends Component {
   render() {
@@ -11,15 +12,8 @@ class App extends Component {
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <HelloWorld defaultName='World' />
       </div>
     );
   }
---
+++ src/components/hellocontent.tsx
@@ -0,0 +1,16 @@
+/// <reference path="./interfaces.d.ts" />
+
+import * as React from "react";
+
+export default class HelloContent extends React.Component<
+  IHelloContentProps,
+  any
+> {
+  constructor(props: IHelloContentProps) {
+    super(props);
+  }
+
+  public render() {
+    return <div>Hello {this.props.name}!</div>;
+  }
+}
